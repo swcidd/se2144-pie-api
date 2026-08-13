d5ec96e40edacea8886e08d058de6ef9ec6d25e4
swcidd <sanol.sherwinsid@gmail.com>
2026-08-13 08:08:39 +0800

    feat: Add router.delete function

--- src/pieRoutes.ts
+++ src/pieRoutes.ts
@@ -53,4 +53,25 @@ router.put("/:id", async (req: Request, res: Response) => {
   }
 });
 
+//DELETE
+router.delete("/:id", async (req: Request, res: Response) => {
+  const { id } = req.params;
+  const { name, crust_type, filling, is_baked, slice_count }: Pie = req.body;
+  try {
+    const result = await pool.query(
+      `DELETE FROM pies
+       SET name = $1, crust_type = $2, filling = $3, is_baked = $4, slice_count = $5
+       WHERE id = $1
+       RETURNING *`,
+      [name, crust_type, filling, is_baked, slice_count, id],
+    );
+    if (result.rows.length === 0) {
+      return res.status(404).json({ error: "Pie not found" });
+    }
+    res.json(result.rows[0]);
+  } catch (error) {
+    res.status(500).json({ error: (error as Error).message });
+  }
+});
+
 export default router;
